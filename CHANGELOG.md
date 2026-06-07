1503d034a44827b3cbbe5ec6e8b6578d1023e06e
loonghao <hal.long@outlook.com>
2026-06-07 10:56:12 +0800

    chore: remove all template code, keep only README files

--- CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# CHANGELOG
-
-
-
-## v0.0.0 (2025-03-19)
-
-### Unknown
-
-* Initial commit ([`b5ec414`](https://github.com/loonghao/dcc-mcp/commit/b5ec414afdfd58333fdf1869d245acfbcd69db89))
--- tests/test_version.py
+++
@@ -1,9 +0,0 @@
-"""Test version information."""
-
-# Import local modules
-from your_project_name import __version__
-
-
-def test_version():
-    """Test that version is a string."""
-    assert isinstance(__version__, str)
